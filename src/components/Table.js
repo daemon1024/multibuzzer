e51cb2130caed5071a1775d64e048610d1bff95f
daemon1024 <barun1024@gmail.com>
2025-12-24 15:04:37 +0530

    Fix ESLint warning for CI builds
    Add eslint-disable-next-line for exhaustive-deps in useEffect.
    The dependencies are intentionally not included as they would cause
    infinite loops.

--- src/components/Table.js
+++ src/components/Table.js
@@ -69,6 +69,7 @@ export default function Table(game) {
     }
 
     queueRef.current = game.G.queue;
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [game.G.queue]);
 
   const attemptBuzz = () => {
